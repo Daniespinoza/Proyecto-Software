3d8338e8a223422724d91f102db0b6bae357f88c
jmaureira <jmaureira@icci.cl>
2017-11-11 02:58:31 -0300

    unique email on users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email');
+            $table->string('email',50);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+        });
+        Schema::table('users', function (Blueprint $table){
+            $table->unique('email');
         });
     }
 

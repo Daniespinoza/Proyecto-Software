7c4863ac8148db1f316eee148b564f5907c8a841
jzagal <zagalvelozo@gmail.com>
2017-11-15 16:34:19 -0300

    Update ExhibitorsController.php

--- app/Http/Controllers/ExhibitorsController.php
+++ app/Http/Controllers/ExhibitorsController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 use App\Exhibitor;
 
 class ExhibitorsController extends Controller
-{
+{   
+    public function show($id)
+    {
+        return view('user.profile', ['user' => User::findOrFail($id)]);
+    }
     //
 }

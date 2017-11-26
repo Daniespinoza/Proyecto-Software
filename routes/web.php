013cb2f646166639241bbc7d828124474bf06c08
jmaureira <jmaureira@icci.cl>
2017-11-26 20:56:55 -0300

    added new routes to exhibitor

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,10 @@ Route::resource('materiales','MaterialesController');
 Route::resource('eventos','EventosController');
 Route::resource('Mis_Datos','DatosController');
 
+Route::get('/mi_horario','DatosController@getHorario')->middleware('exhi');
+Route::get('/ingresar_horario','DatosController@setHorario')->middleware('exhi');
+
+//Route::get('/mi_sueldo','DatosController@getSueldo')->middleware('exhi');
 
 
 

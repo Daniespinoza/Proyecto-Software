908f2e5b3d2d77690ae5e2e25566b8b39d49e429
jzagal <zagalvelozo@gmail.com>
2017-11-27 11:02:02 -0300

    Add files via upload

---
+++ resources/views/eventos/calendar.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.master')
+<link href='css/fullcalendar.min.css' rel='stylesheet' />
+<link href='css/fullcalendar.print.min.css' rel='stylesheet' media='print' />
+<script src='js/moment.min.js'></script>
+<script src='js/jquery.min.js'></script>
+<script src='js/fullcalendar.min.js'></script>
+<script>
+
+	$(document).ready(function() {
+
+		$('#calendar').fullCalendar({
+			defaultDate: '2017-11-12',
+			editable: true,
+      navLinks: true,
+      selectable: true,
+			eventLimit: true, // allow "more" link when too many events
+			events: [
+				{
+					title: 'Expo 1',
+					start: '2017-11-01'
+				},
+				{
+					title: 'Semana de exposiones',
+					start: '2017-11-07',
+					end: '2017-11-10'
+				},
+				{
+					id: 999,
+					title: 'Expo 2',
+					start: '2017-11-09T16:00:00'
+				},
+				{
+					id: 999,
+					title: 'Expo Mapocho',
+					start: '2017-11-16T16:00:00'
+				}
+			]
+		});
+
+	});
+
+</script>
+@section('title','Calendario')
+@section('ventana','Calendario')
+@section('contenido')
+
+<div id='calendar'></div>
+
+@endsection

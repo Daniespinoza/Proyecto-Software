31bb548caad3e07a4a9cc6372044e680b5b437b7
jzagal <zagalvelozo@gmail.com>
2017-12-02 06:38:58 -0300

    Update Event.php
    TO DO!

--- app/Event.php
+++ app/Event.php
@@ -8,6 +8,8 @@ class Event extends Model
 {
   protected $primaryKey = 'id';
 
+  // TODO: Crear campo title, start, luego retornar con un json con     $event = App\Event::all(); return $event->toJson();
+  
   protected $fillable = [
     'id_tipo_evento',
     'id_personal',

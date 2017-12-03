5c97af6ffee8489d3906eb25ff5116f59cc159dd
jmaureira <jmaureira@icci.cl>
2017-12-03 16:58:35 -0300

    fixed seeder event

--- database/seeds/EventTableSeeder.php
+++ database/seeds/EventTableSeeder.php
@@ -15,6 +15,7 @@ class EventTableSeeder extends Seeder
       $evento->id_tipo_evento = 1;
       $evento->id_personal = 2;
       $evento->id_establecimiento = 1 ;
+      $evento->direccion = 'Av. Maipú #456';
       $evento->cupos=4;
       $evento->fecha_inicio = '2017-11-10' ;
       $evento -> save();
@@ -23,6 +24,7 @@ class EventTableSeeder extends Seeder
       $evento1->id_tipo_evento = 3;
       $evento1->id_personal = 2;
       $evento1->id_establecimiento = 2 ;
+      $evento1->direccion = 'Av. Matta #5603';
       $evento1->cupos=4;
       $evento1->fecha_inicio = '2017-11-15' ;
       $evento1 -> save();

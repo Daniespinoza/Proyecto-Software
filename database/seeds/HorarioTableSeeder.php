0bf98e1cd889036ab98dc471217112d1fd256b90
jmaureira <jmaureira@icci.cl>
2017-12-10 04:23:14 -0300

    Expositor seeders added

--- database/seeds/HorarioTableSeeder.php
+++ database/seeds/HorarioTableSeeder.php
@@ -25,5 +25,135 @@ class HorarioTableSeeder extends Seeder
         $horario->total_t = 2;
         $horario->save();
 
+        $horario = new Disponibilidad();
+        $horario->id_expositor = 2;
+        $horario->lunes = 'Ninguno' ;
+        $horario->martes = 'Mañana' ;
+        $horario->miercoles = 'Ninguno' ;
+        $horario->jueves = 'Tarde' ;
+        $horario->viernes = 'Todo el Día' ;
+        $horario->sabado = 'Ninguno';
+        $horario->domingo = 'Ninguno';
+        $horario->total_m = 2;
+        $horario->total_t = 2;
+        $horario->save();
+
+        $horario = new Disponibilidad();
+        $horario->id_expositor = 3;
+        $horario->lunes = 'Ninguno' ;
+        $horario->martes = 'Todo el Día' ;
+        $horario->miercoles = 'Todo el Día' ;
+        $horario->jueves = 'Tarde' ;
+        $horario->viernes = 'Todo el Día' ;
+        $horario->sabado = 'Ninguno';
+        $horario->domingo = 'Ninguno';
+        $horario->total_m = 3;
+        $horario->total_t = 4;
+        $horario->save();
+
+        $horario = new Disponibilidad();
+        $horario->id_expositor = 4;
+        $horario->lunes = 'Mañana' ;
+        $horario->martes = 'Mañana' ;
+        $horario->miercoles = 'Mañana' ;
+        $horario->jueves = 'Tarde' ;
+        $horario->viernes = 'Todo el Día' ;
+        $horario->sabado = 'Mañana';
+        $horario->domingo = 'Mañana';
+        $horario->total_m = 6;
+        $horario->total_t = 2;
+        $horario->save();
+
+        $horario = new Disponibilidad();
+        $horario->id_expositor = 5;
+        $horario->lunes = 'Todo el Día' ;
+        $horario->martes = 'Mañana' ;
+        $horario->miercoles = 'Mañana' ;
+        $horario->jueves = 'Tarde' ;
+        $horario->viernes = 'Todo el Día' ;
+        $horario->sabado = 'Todo el Día';
+        $horario->domingo = 'Ninguno';
+        $horario->total_m = 5;
+        $horario->total_t = 4;
+        $horario->save();
+
+        $horario = new Disponibilidad();
+        $horario->id_expositor = 6;
+        $horario->lunes = 'Tarde' ;
+        $horario->martes = 'Mañana' ;
+        $horario->miercoles = 'Tarde' ;
+        $horario->jueves = 'Tarde' ;
+        $horario->viernes = 'Mañana' ;
+        $horario->sabado = 'Mañana';
+        $horario->domingo = 'Ninguno';
+        $horario->total_m = 3;
+        $horario->total_t = 3;
+        $horario->save();
+
+        $horario = new Disponibilidad();
+        $horario->id_expositor = 7;
+        $horario->lunes = 'Todo el Día' ;
+        $horario->martes = 'Todo el Día' ;
+        $horario->miercoles = 'Todo el Día' ;
+        $horario->jueves = 'Todo el Día' ;
+        $horario->viernes = 'Ninguno' ;
+        $horario->sabado = 'Ninguno';
+        $horario->domingo = 'Ninguno';
+        $horario->total_m = 4;
+        $horario->total_t = 4;
+        $horario->save();
+
+        $horario = new Disponibilidad();
+        $horario->id_expositor = 8;
+        $horario->lunes ='Tarde' ;
+        $horario->martes = 'Tarde' ;
+        $horario->miercoles = 'Tarde' ;
+        $horario->jueves = 'Tarde' ;
+        $horario->viernes = 'Todo el Día' ;
+        $horario->sabado = 'Ninguno';
+        $horario->domingo = 'Ninguno';
+        $horario->total_m = 1;
+        $horario->total_t = 5;
+        $horario->save();
+
+        $horario = new Disponibilidad();
+        $horario->id_expositor = 9;
+        $horario->lunes = 'Mañana' ;
+        $horario->martes = 'Tarde' ;
+        $horario->miercoles = 'Mañana' ;
+        $horario->jueves = 'Ninguno' ;
+        $horario->viernes = 'Todo el Día' ;
+        $horario->sabado = 'Ninguno';
+        $horario->domingo = 'Ninguno';
+        $horario->total_m = 3;
+        $horario->total_t = 1;
+        $horario->save();
+
+        $horario = new Disponibilidad();
+        $horario->id_expositor = 10;
+        $horario->lunes = 'Mañana' ;
+        $horario->martes = 'Mañana' ;
+        $horario->miercoles = 'Mañana' ;
+        $horario->jueves = 'Mañana' ;
+        $horario->viernes = 'Mañana' ;
+        $horario->sabado = 'Mañana';
+        $horario->domingo = 'Ninguno';
+        $horario->total_m = 6;
+        $horario->total_t = 0;
+        $horario->save();
+
+        $horario = new Disponibilidad();
+        $horario->id_expositor = 11;
+        $horario->lunes = 'Todo el Día' ;
+        $horario->martes = 'Todo el Día' ;
+        $horario->miercoles = 'Todo el Día' ;
+        $horario->jueves = 'Todo el Día' ;
+        $horario->viernes = 'Todo el Día' ;
+        $horario->sabado = 'Ninguno';
+        $horario->domingo = 'Ninguno';
+        $horario->total_m = 5;
+        $horario->total_t = 5;
+        $horario->save();
+
     }
 }
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -44,5 +44,86 @@ class UserTableSeeder extends Seeder
       $expositor->activo = true;
       $expositor->save();
 
+      $expositor1 = new User();
+      $expositor1->name = 'Expositor1';
+      $expositor1->email = 'expositor1@utem.cl';
+      $expositor1->password = bcrypt('expositor1');
+      $expositor1->id_rol = 4;
+      $expositor1->activo = true;
+      $expositor1->save();
+
+      $expositor2 = new User();
+      $expositor2->name = 'Expositor2';
+      $expositor2->email = 'expositor2@utem.cl';
+      $expositor2->password = bcrypt('expositor2');
+      $expositor2->id_rol = 4;
+      $expositor2->activo = true;
+      $expositor2->save();
+
+      $expositor3 = new User();
+      $expositor3->name = 'Expositor3';
+      $expositor3->email = 'expositor3@utem.cl';
+      $expositor3->password = bcrypt('expositor3');
+      $expositor3->id_rol = 4;
+      $expositor3->activo = true;
+      $expositor3->save();
+
+      $expositor4 = new User();
+      $expositor4->name = 'Expositor4';
+      $expositor4->email = 'expositor4@utem.cl';
+      $expositor4->password = bcrypt('expositor4');
+      $expositor4->id_rol = 4;
+      $expositor4->activo = true;
+      $expositor4->save();
+
+      $expositor5 = new User();
+      $expositor5->name = 'Expositor5';
+      $expositor5->email = 'expositor5@utem.cl';
+      $expositor5->password = bcrypt('expositor5');
+      $expositor5->id_rol = 4;
+      $expositor5->activo = true;
+      $expositor5->save();
+
+      $expositor6 = new User();
+      $expositor6->name = 'Expositor6';
+      $expositor6->email = 'expositor6@utem.cl';
+      $expositor6->password = bcrypt('expositor6');
+      $expositor6->id_rol = 4;
+      $expositor6->activo = true;
+      $expositor6->save();
+
+      $expositor7 = new User();
+      $expositor7->name = 'Expositor7';
+      $expositor7->email = 'expositor7@utem.cl';
+      $expositor7->password = bcrypt('expositor7');
+      $expositor7->id_rol = 4;
+      $expositor7->activo = true;
+      $expositor7->save();
+
+      $expositor8 = new User();
+      $expositor8->name = 'Expositor8';
+      $expositor8->email = 'expositor8@utem.cl';
+      $expositor8->password = bcrypt('expositor8');
+      $expositor8->id_rol = 4;
+      $expositor8->activo = true;
+      $expositor8->save();
+
+      $expositor9 = new User();
+      $expositor9->name = 'Expositor9';
+      $expositor9->email = 'expositor9@utem.cl';
+      $expositor9->password = bcrypt('expositor9');
+      $expositor9->id_rol = 4;
+      $expositor9->activo = true;
+      $expositor9->save();
+
+      $expositor0 = new User();
+      $expositor0->name = 'Expositor0';
+      $expositor0->email = 'expositor0@utem.cl';
+      $expositor0->password = bcrypt('expositor0');
+      $expositor0->id_rol = 4;
+      $expositor0->activo = true;
+      $expositor0->save();
+
+
     }
 }

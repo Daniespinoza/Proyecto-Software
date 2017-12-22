bf10018cc4c2b58a78f22077faecd78df3f9a848
Daniespinoza <daniel.espinozac@utem.cl>
2017-12-22 02:02:21 -0300

    comunas quinta region

--- database/seeds/CommuneTableSeeder.php
+++ database/seeds/CommuneTableSeeder.php
@@ -200,5 +200,190 @@ class CommuneTableSeeder extends Seeder
       $comuna333->id_region = 1;
       $comuna333->save();
 
+      $comuna3331 = new Commune();
+      $comuna3331->nombre = "Isla de Pascua";
+      $comuna3331->id_region = 2;
+      $comuna3331->save();
+
+      $comuna3332 = new Commune();
+      $comuna3332->nombre = "Calle Larga";
+      $comuna3332->id_region = 2;
+      $comuna3332->save();
+
+      $comuna3333 = new Commune();
+      $comuna3333->nombre = "Los Andes";
+      $comuna3333->id_region = 2;
+      $comuna3333->save();
+
+      $comuna3334 = new Commune();
+      $comuna3334->nombre = "Rinconada";
+      $comuna3334->id_region = 2;
+      $comuna3334->save();
+
+      $comuna3335 = new Commune();
+      $comuna3335->nombre = "San Esteban";
+      $comuna3335->id_region = 2;
+      $comuna3335->save();
+
+      $comuna3336 = new Commune();
+      $comuna3336->nombre = "Cabildo";
+      $comuna3336->id_region = 2;
+      $comuna3336->save();
+
+      $comuna3337 = new Commune();
+      $comuna3337->nombre = "La Ligua";
+      $comuna3337->id_region = 2;
+      $comuna3337->save();
+
+      $comuna3338 = new Commune();
+      $comuna3338->nombre = "Papudo";
+      $comuna3338->id_region = 2;
+      $comuna3338->save();
+
+      $comuna3339 = new Commune();
+      $comuna3339->nombre = "Petorca";
+      $comuna3339->id_region = 2;
+      $comuna3339->save();
+
+      $comuna3340 = new Commune();
+      $comuna3340->nombre = "Zapallar";
+      $comuna3340->id_region = 2;
+      $comuna3340->save();
+
+      $comuna3341 = new Commune();
+      $comuna3341->nombre = "Hijuelas";
+      $comuna3341->id_region = 2;
+      $comuna3341->save();
+
+      $comuna3342 = new Commune();
+      $comuna3342->nombre = "La Cruz";
+      $comuna3342->id_region = 2;
+      $comuna3342->save();
+
+      $comuna3343 = new Commune();
+      $comuna3343->nombre = "Nogales";
+      $comuna3343->id_region = 2;
+      $comuna3343->save();
+
+      $comuna3344 = new Commune();
+      $comuna3344->nombre = "Quillota";
+      $comuna3344->id_region = 2;
+      $comuna3344->save();
+
+      $comuna3345 = new Commune();
+      $comuna3345->nombre = "Algarrobo";
+      $comuna3345->id_region = 2;
+      $comuna3345->save();
+
+      $comuna3346 = new Commune();
+      $comuna3346->nombre = "Cartagena";
+      $comuna3346->id_region = 2;
+      $comuna3346->save();
+
+      $comuna3347 = new Commune();
+      $comuna3347->nombre = "El Quisco";
+      $comuna3347->id_region = 2;
+      $comuna3347->save();
+
+      $comuna3348 = new Commune();
+      $comuna3348->nombre = "El Tabo";
+      $comuna3348->id_region = 2;
+      $comuna3348->save();
+
+      $comuna3349 = new Commune();
+      $comuna3349->nombre = "San Antonio";
+      $comuna3349->id_region = 2;
+      $comuna3349->save();
+
+      $comuna3350 = new Commune();
+      $comuna3350->nombre = "Santo Domingo";
+      $comuna3350->id_region = 2;
+      $comuna3350->save();
+
+      $comuna3351 = new Commune();
+      $comuna3351->nombre = "Catemu";
+      $comuna3351->id_region = 2;
+      $comuna3351->save();
+
+      $comuna3352 = new Commune();
+      $comuna3352->nombre = "Llay-Llay";
+      $comuna3352->id_region = 2;
+      $comuna3352->save();
+
+      $comuna3353 = new Commune();
+      $comuna3353->nombre = "Panquehue";
+      $comuna3353->id_region = 2;
+      $comuna3353->save();
+
+      $comuna3354 = new Commune();
+      $comuna3354->nombre = "Putaendo";
+      $comuna3354->id_region = 2;
+      $comuna3354->save();
+
+      $comuna3355 = new Commune();
+      $comuna3355->nombre = "San Felipe";
+      $comuna3355->id_region = 2;
+      $comuna3355->save();
+
+      $comuna3356 = new Commune();
+      $comuna3356->nombre = "Santa María";
+      $comuna3356->id_region = 2;
+      $comuna3356->save();
+
+      $comuna3357 = new Commune();
+      $comuna3357->nombre = "Casablanca";
+      $comuna3357->id_region = 2;
+      $comuna3357->save();
+
+      $comuna3358 = new Commune();
+      $comuna3358->nombre = "Concón";
+      $comuna3358->id_region = 2;
+      $comuna3358->save();
+
+      $comuna3359 = new Commune();
+      $comuna3359->nombre = "Juan Fernández";
+      $comuna3359->id_region = 2;
+      $comuna3359->save();
+
+      $comuna3360 = new Commune();
+      $comuna3360->nombre = "Puchuncaví";
+      $comuna3360->id_region = 2;
+      $comuna3360->save();
+
+      $comuna3361 = new Commune();
+      $comuna3361->nombre = "Quintero";
+      $comuna3361->id_region = 2;
+      $comuna3361->save();
+
+      $comuna3362 = new Commune();
+      $comuna3362->nombre = "Valparaíso";
+      $comuna3362->id_region = 2;
+      $comuna3362->save();
+
+      $comuna3363 = new Commune();
+      $comuna3363->nombre = "Viña del Mar";
+      $comuna3363->id_region = 2;
+      $comuna3363->save();
+
+      $comuna3364 = new Commune();
+      $comuna3364->nombre = "Limache";
+      $comuna3364->id_region = 2;
+      $comuna3364->save();
+
+      $comuna3365 = new Commune();
+      $comuna3365->nombre = "Olmué";
+      $comuna3365->id_region = 2;
+      $comuna3365->save();
+
+      $comuna3366 = new Commune();
+      $comuna3366->nombre = "Quilpué";
+      $comuna3366->id_region = 2;
+      $comuna3366->save();
+
+      $comuna3367 = new Commune();
+      $comuna3367->nombre = "Villa Alemana";
+      $comuna3367->id_region = 2;
+      $comuna3367->save();
+
     }
 }

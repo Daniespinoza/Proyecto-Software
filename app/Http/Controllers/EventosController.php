8024cdf1495636d018df63667d7417bc75d95e25
Daniespinoza <31497123+Daniespinoza@users.noreply.github.com>
2017-12-23 12:27:40 -0300

    Update EventosController.php

--- app/Http/Controllers/EventosController.php
+++ app/Http/Controllers/EventosController.php
@@ -298,7 +298,7 @@ class EventosController extends Controller
         $ev = Event::find($id);
         //dd($ev['start']);
         $expositores = Exhibitor::orderBy('alu_nombre','asc')->where('activo','=',1)->get();
-        dd($expositores);
+        //dd($expositores);
         $detalles = Turndetail::all()->toArray();
         $disponibilidades = Disponibilidad::all()->toArray();
 
